c888982972c5d268dfefe5fdc003f2e1bba6e637
matteo08 <121456471+mmatrangolo@users.noreply.github.com>
2023-01-22 15:23:41 +0000

    UI

--- main.py
+++ main.py
@@ -6,20 +6,39 @@ b = 1 / sqrt(2)
 t = 1 / 4
 p = 1
 
+def glpi(n):
+
+  print('')
+
+  a = 1
+  b = 1 / sqrt(2)
+  t = 1 / 4
+  p = 1
+    
+  for i in range (1, n + 1):
+    acurr = (a + b) / 2
+    bcurr = sqrt(a * b)
+    tcurr = t - p * pow((a - acurr), 2)
+    pcurr = 2 * p
+    pi = pow((acurr + bcurr), 2) / (4 * tcurr)
+    print(str(i) + '   ' + str(pi))
+        
+    a = acurr
+    b = bcurr
+    t = tcurr
+    p = pcurr
+
 print('Choose a Pi between:')
+print('')
 print('1. Gauss-Legendre Algorithm')
+print('')
 piIn = (int(input()))
+print('')
+print('How many Iterations would you like to calculate?')
+print('')
+iter = int(input())
+print('')
 
 if piIn == 1:
-  for i in range (1,21):
-      acurr = (a + b) / 2
-      bcurr = sqrt(a * b)
-      tcurr = t - p * pow((a - acurr),2)
-      pcurr = 2 * p
-      pi = pow((acurr + bcurr),2) / (4 * tcurr)
-      print(str(i) + '   ' + str(pi))
-      
-      a = acurr
-      b = bcurr
-      t = tcurr
-      p = pcurr
+  glpi(iter)
+
